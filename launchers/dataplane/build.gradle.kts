1e5bd2db5a6abcb44bb2882ba663a8131527dd74
Brais <braisgmenu@gmail.com>
2025-07-28 17:33:15 +0200

    Initial commit

---
+++ launchers/dataplane/build.gradle.kts
@@ -0,0 +1,49 @@
+/*
+*  Copyright (c) 2023 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
+*
+*  This program and the accompanying materials are made available under the
+*  terms of the Apache License, Version 2.0 which is available at
+*  https://www.apache.org/licenses/LICENSE-2.0
+*
+*  SPDX-License-Identifier: Apache-2.0
+*
+*  Contributors:
+*       Bayerische Motoren Werke Aktiengesellschaft (BMW AG) - Initial API and Implementation
+*
+*/
+
+plugins {
+    `java-library`
+    id("application")
+    alias(libs.plugins.shadow)
+}
+
+dependencies {
+    runtimeOnly(libs.edc.bom.dataplane)
+    runtimeOnly(libs.edc.dataplane.v2)
+
+    //monitor custom
+    runtimeOnly(project(":extensions:monitor-custom"))
+
+    if (project.properties.getOrDefault("persistence", "false") == "true") {
+        runtimeOnly(libs.edc.vault.hashicorp)
+        runtimeOnly(libs.edc.bom.dataplane.sql)
+        println("This runtime compiles with a remote STS client, Hashicorp Vault and PostgreSQL. You will need properly configured Postgres and HCV instances.")
+    }
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    exclude("**/pom.properties", "**/pom.xml")
+    mergeServiceFiles()
+    archiveFileName.set("${project.name}.jar")
+}
+tasks.named<JavaExec>("run") {
+    args("--runtime-id=data-plane") // Cambia "control-plane" por el ID que corresponda
+}
+application {
+    mainClass.set("org.eclipse.edc.monitor.runtime.CustomBaseRuntime")
+}
+
+edcBuild {
+    publish.set(false)
+}
---
+++ launchers/issuerservice/build.gradle.kts
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (c) 2025 Cofinity-X
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Cofinity-X - initial API and implementation
+ *
+ */
+
+plugins {
+    `java-library`
+    id("application")
+    alias(libs.plugins.shadow)
+}
+
+
+dependencies {
+    implementation(libs.edc.issuance.spi) // for seeding the attestations
+    runtimeOnly(project(":extensions:superuser-seed"))
+    runtimeOnly(libs.edc.bom.issuerservice)
+    runtimeOnly(libs.edc.ih.api.did)
+    runtimeOnly(libs.edc.ih.api.participants)
+
+    runtimeOnly(project(":extensions:monitor-custom"))
+
+    if (project.properties.getOrDefault("persistence", "false") == "true") {
+        runtimeOnly(libs.edc.vault.hashicorp)
+        runtimeOnly(libs.edc.bom.issuerservice.sql)
+        println("This runtime compiles with a remote STS client, Hashicorp Vault and PostgreSQL. You will need properly configured Postgres and HCV instances.")
+    }
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    exclude("**/pom.properties", "**/pom.xml")
+    mergeServiceFiles()
+    archiveFileName.set("issuerservice.jar")
+}
+tasks.named<JavaExec>("run") {
+    args("--runtime-id=issuer-service") // Cambia "control-plane" por el ID que corresponda
+}
+application {
+    mainClass.set("org.eclipse.edc.monitor.runtime.CustomBaseRuntime")
+}
+
+edcBuild {
+    publish.set(false)
+}
