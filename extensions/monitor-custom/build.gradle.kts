7d9ae7ca7670b549aa623e3d747350362a534991
Brais <braisgmenu@gmail.com>
2025-07-29 16:01:12 +0200

    database para logs funcionando

--- extensions/monitor-custom/build.gradle.kts
+++ extensions/monitor-custom/build.gradle.kts
@@ -15,5 +15,6 @@ dependencies {
     // Necesitas esta dependencia para poder implementar la interfaz "Monitor"
     implementation(libs.edc.spi.core)
     implementation(libs.edc.boot)
+    runtimeOnly(libs.postgres)
 
 }
---
+++ extensions/monitor-custom/src/main/java/org/eclipse/edc/monitor/DatabaseMonitorExtension.java
@@ -0,0 +1,56 @@
+package org.eclipse.edc.monitor;
+
+
+import org.eclipse.edc.spi.monitor.Monitor;
+import org.eclipse.edc.spi.system.ServiceExtension;
+import org.eclipse.edc.spi.system.ServiceExtensionContext;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Esta clase extiende la funcionalidad del sistema de monitoreo utilizando una base de datos.
+ */
+public class DatabaseMonitorExtension implements ServiceExtension {
+    private DatabasMonitor newMonitor;
+
+    @Override
+    public String name() {
+        return "Database Monitor";
+    }
+
+    @Override
+    public void initialize(ServiceExtensionContext context) {
+        var originalMonitor = context.getMonitor();
+        var config = context.getConfig("edc.datasource.log");
+
+
+
+        String url = config.getString("url");
+        String user = config.getString("user");
+        String password = config.getString("password");
+
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            var runtimeId = context.getRuntimeId();
+
+            this.newMonitor = new DatabasMonitor(runtimeId, originalMonitor, connection);
+            context.registerService(Monitor.class, newMonitor);
+            originalMonitor.info("Database Monitor registered for runtime: " + runtimeId);
+
+        } catch (SQLException e) {
+            originalMonitor.severe("NO SE PUDO CONECTAR CON LA DATABASE", e);
+            throw new RuntimeException(e);
+
+        }
+
+    }
+
+    @Override
+    public void shutdown() {
+        if (newMonitor != null) {
+            newMonitor.close();
+        }
+    }
+}
